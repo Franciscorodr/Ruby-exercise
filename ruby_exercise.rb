8eca2a4bf84f1086bb583cef37cbeda6ea4cccff
Fran Bolívar Lupiáñez <franciscojavierbolivarlupianez@gmail.com>
2018-10-03 17:05:56 +0200

    Some ruby good practices

--- ruby_exercise.rb
+++ ruby_exercise.rb
@@ -1,30 +1,30 @@
 require 'nokogiri'
 require 'open-uri'
 
-parseProduct = Proc.new { |productInfo|
+parse_product = Proc.new do |product_info|
 	product = Hash.new
 	# Monitors
-	product['monitors'] = productInfo.css('h2').text.to_i
+	product[:monitors] = product_info.css('h2').text.to_i
 	# Thin block info
-	thinInfo = productInfo.css('dl.thin > dd')
+	thin_info = product_info.css('dl.thin > dd')
 	# Thin info - check_rate
-	product['check_rate'] = thinInfo[0].text.match(/\d+/)[0].to_i
+	product[:check_rate] = thin_info[0].text.match(/\d+/)[0].to_i
 	# Thin info - history
-	product['history'] = thinInfo[1].text.match(/\d+/)[0].to_i
+	product[:history] = thin_info[1].text.match(/\d+/)[0].to_i
 	# Thin info - multiple_notifications
-	product['multiple_notifications'] = thinInfo[2].css('span').attr('class').text.include? 'success'
+	product[:multiple_notifications] = thin_info[2].css('span').attr('class').text.include? 'success'
 	# Thin info - push_notifications
-	product['push_notifications'] = thinInfo[3].css('span').attr('class').text.include? 'success'
+	product[:push_notifications] = thin_info[3].css('span').attr('class').text.include? 'success'
 	# Price
-	product['price'] = /\d+\.\d+/.match(productInfo.css('.btn').text)[0].to_f
+	product[:price] = /\d+\.\d+/.match(product_info.css('.btn').text)[0].to_f
 	product
-}
+end
 
 url = 'https://www.port-monitor.com/plans-and-pricing'
 
 document = Nokogiri::HTML(open(url))
 
 products = document.css('.product')
-finalList = products.map(&parseProduct)
+final_list = products.map(&parse_product)
 
-puts finalList
+puts final_list
